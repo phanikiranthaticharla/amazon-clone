e46a2427a55a06f922c0aa1f21fc511b7de9418a
Phani Kiran Thaticharla <tphanikiran@gmail.com>
2023-02-01 09:38:03 -0500

    Added Searchbar and Search Icon

--- src/Components/NavBar/Navigation.js
+++ src/Components/NavBar/Navigation.js
@@ -10,11 +10,28 @@ class NavBar extends Component {
     render() { 
         return ( 
         <div className="navbar__component">
-            Nav Bar
             <div className="navbar__logo"></div>
             <div className='navbar__locator'>
                 <div className="navbar__locatorImage"></div>
-                <div className='navbar_location'>US</div>
+                <div className='navbar__location'>US</div>
+            </div>
+            <div className="navbar__searchcomponent">
+                <div className="navbar__dropdown">
+                    <select>
+                        <option value="">All</option>
+                        <option value="">Alexa</option>
+                        <option value="">Baby</option>
+                        <option value="">Beauty</option>
+                        <option value="">Books</option>
+                        <option value="">Clothes</option>
+                    </select>
+                </div>
+                <div>
+                    <input type="text" className="navbar__searchbox" />
+                </div>
+                <div className="navbar__searchboxdiv">
+                    <div className="navbar__searchicon"></div>
+                </div>
             </div>
         </div> );
     }
